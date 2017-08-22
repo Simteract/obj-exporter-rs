b4d8ab3c754f43e9b30f98f965cc04489da183c0
Adam Rzepka <adam.rzepka@coddee.co>
2017-08-22 10:43:37 +0200

    Test multiple objects

--- tests/tests.rs
+++ tests/tests.rs
@@ -15,11 +15,11 @@ pub fn test_square() {
             (1, 1, 0),
             (-1, 1, 0)
           ].into_iter().map(|(x, y, z)| {
-                Vertex {
-                    x: x as f64,
-                    y: y as f64,
-                    z: z as f64,
-                }
+            Vertex {
+              x: x as f64,
+              y: y as f64,
+              z: z as f64,
+            }
           }).collect(),
           tex_vertices: vec![],
           normals: vec![],
@@ -208,6 +208,106 @@ vt 0.000000 1.000000
 vn 0.000000 0.000000 -1.000000
 f 1/1/1 2/2/1 3/3/1
 f 1/1/1 3/3/1 4/4/1
+"#;
+
+    let result = obj::export(&set);
+    assert_eq!(result, expected);
+}
+
+#[test]
+pub fn test_multiple_objects() {
+    let set = ObjSet {
+      material_library: None,
+      objects: vec![
+        Object {
+          name: "Square1".to_owned(),
+          vertices: vec![
+            (-1, -1, 0),
+            (1, -1, 0),
+            (1, 1, 0),
+            (-1, 1, 0)
+          ].into_iter().map(|(x, y, z)| {
+            Vertex {
+              x: x as f64,
+              y: y as f64,
+              z: z as f64,
+            }
+          }).collect(),
+          tex_vertices: vec![],
+          normals: vec![],
+          geometry: vec![
+            Geometry {
+              material_name: None,
+              shapes: vec![
+                (0, 1, 2),
+                (0, 2, 3)
+              ].into_iter().map(|(x, y, z)| {
+                Shape {
+                  primitive: Primitive::Triangle(
+                    (x, None, None),
+                    (y, None, None),
+                    (z, None, None)
+                  ),
+                  groups: vec![],
+                  smoothing_groups: vec![],
+                }
+              }).collect(),
+            }
+          ],
+        },
+        Object {
+          name: "Square2".to_owned(),
+          vertices: vec![
+            (1, -1, 0),
+            (2, -1, 0),
+            (2, 1, 0),
+            (1, 1, 0)
+          ].into_iter().map(|(x, y, z)| {
+            Vertex {
+              x: x as f64,
+              y: y as f64,
+              z: z as f64,
+            }
+          }).collect(),
+          tex_vertices: vec![],
+          normals: vec![],
+          geometry: vec![
+            Geometry {
+              material_name: None,
+              shapes: vec![
+                (0, 1, 2),
+                (0, 2, 3)
+              ].into_iter().map(|(x, y, z)| {
+                Shape {
+                  primitive: Primitive::Triangle(
+                    (x, None, None),
+                    (y, None, None),
+                    (z, None, None)
+                  ),
+                  groups: vec![],
+                  smoothing_groups: vec![],
+                }
+              }).collect(),
+            }
+          ],
+        }
+      ],
+    };
+
+    let expected = r#"o Square1
+v -1.000000 -1.000000 0.000000
+v 1.000000 -1.000000 0.000000
+v 1.000000 1.000000 0.000000
+v -1.000000 1.000000 0.000000
+f 1 2 3
+f 1 3 4
+o Square2
+v 1.000000 -1.000000 0.000000
+v 2.000000 -1.000000 0.000000
+v 2.000000 1.000000 0.000000
+v 1.000000 1.000000 0.000000
+f 1 2 3
+f 1 3 4
 "#;
 
     let result = obj::export(&set);
