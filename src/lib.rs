a19c2a848951e29d8dcac543db300b432af50712
Adam Rzepka <adam.rzepka@coddee.co>
2017-08-22 10:22:28 +0200

    UVs

--- src/lib.rs
+++ src/lib.rs
@@ -19,20 +19,38 @@ pub fn export_to_file(_obj_set: &ObjSet, _path: &Path) -> Result<(), io::Error> 
 fn serialize_object(object: &Object) -> String {
     let name = format!("o {}\n", object.name);
     let vertices = object.vertices.iter().map(serialize_vertex).join("");
+    let uvs = object.tex_vertices.iter().map(serialize_uv).join("");
     assert_eq!(object.geometry.len(), 1);
     let faces = object.geometry[0].shapes.iter().map(serialize_shape).join("");
-    format!("{}{}{}", name, vertices, faces)
+    format!("{}{}{}{}", name, vertices, uvs, faces)
 }
 
 fn serialize_vertex(v: &Vertex) -> String {
     format!("v {:.6} {:.6} {:.6}\n", v.x, v.y, v.z)
 }
 
+fn serialize_uv(uv: &TVertex) -> String {
+    if uv.w == 0.0 {
+        format!("vt {:.6} {:.6}\n", uv.u, uv.v)
+    } else {
+        format!("vt {:.6} {:.6} {:.6}\n", uv.u, uv.v, uv.w)
+    }
+}
+
 fn serialize_shape(shape: &Shape) -> String {
     match shape.primitive {
-        Primitive::Point((vi, _, _)) => format!("f {}\n", vi + 1),
-        Primitive::Line((vi1, _, _), (vi2, _, _)) => format!("f {} {}\n", vi1 + 1, vi2 + 1),
-        Primitive::Triangle((vi1, _, _), (vi2, _, _), (vi3, _, _)) => format!("f {} {} {}\n", vi1 + 1, vi2 + 1, vi3 + 1),
+        Primitive::Point(vtn) => format!("f {}\n", serialize_vtn(vtn)),
+        Primitive::Line(vtn1, vtn2) => format!("f {} {}\n", serialize_vtn(vtn1), serialize_vtn(vtn2)),
+        Primitive::Triangle(vtn1, vtn2, vtn3) => format!("f {} {} {}\n", serialize_vtn(vtn1), serialize_vtn(vtn2), serialize_vtn(vtn3)),
+    }
+}
+
+fn serialize_vtn(vtn: VTNIndex) -> String {
+    match vtn {
+        (vi, None, None) => format!("{}", vi + 1),
+        (vi, Some(ti), None) => format!("{}/{}", vi + 1, ti + 1),
+        (vi, Some(ti), Some(ni)) => format!("{}/{}/{}", vi + 1, ti + 1, ni + 1),
+        (vi, None, Some(ni)) => format!("{}//{}", vi + 1, ni + 1),
     }
 }
 
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,12 +1,61 @@
 extern crate obj_exporter as obj;
 
-use obj::{ObjSet, Object, Vertex, Geometry, Shape, Primitive};
+use obj::{ObjSet, Object, Vertex, TVertex, Geometry, Shape, Primitive};
 
 #[test]
 pub fn test_square() {
     let set = ObjSet {
-      material_library: None,
-      objects: vec![
+        material_library: None,
+        objects: vec![Object {
+                          name: "Square".to_owned(),
+                          vertices: vec![(-1, -1, 0), (1, -1, 0), (1, 1, 0), (-1, 1, 0)]
+                              .into_iter()
+                              .map(|(x, y, z)| {
+                    Vertex {
+                        x: x as f64,
+                        y: y as f64,
+                        z: z as f64,
+                    }
+                })
+                              .collect(),
+                          tex_vertices: vec![],
+                          normals: vec![],
+                          geometry: vec![Geometry {
+                                             material_name: None,
+                                             shapes: vec![(0, 1, 2), (0, 2, 3)]
+                                                 .into_iter()
+                                                 .map(|(x, y, z)| {
+                        Shape {
+                            primitive: Primitive::Triangle((x, None, None),
+                                                           (y, None, None),
+                                                           (z, None, None)),
+                            groups: vec![],
+                            smoothing_groups: vec![],
+                        }
+                    })
+                                                 .collect(),
+                                         }],
+                      }],
+    };
+
+    let expected = r#"o Square
+v -1.000000 -1.000000 0.000000
+v 1.000000 -1.000000 0.000000
+v 1.000000 1.000000 0.000000
+v -1.000000 1.000000 0.000000
+f 1 2 3
+f 1 3 4
+"#;
+
+    let result = obj::export(&set);
+    assert_eq!(result, expected);
+}
+
+#[test]
+pub fn test_square_with_uv() {
+    let set = ObjSet {
+        material_library: None,
+        objects: vec![
         Object {
           name: "Square".to_owned(),
           vertices:
@@ -22,7 +71,18 @@ pub fn test_square() {
                 z: z as f64
               })
             .collect(),
-          tex_vertices: vec!(),
+          tex_vertices: vec![
+            (0, 0),
+            (1, 0),
+            (1, 1),
+            (0, 1),
+          ].into_iter().map(|(u, v)|
+            TVertex {
+              u: u as f64,
+              v: v as f64,
+              w: 0.0,
+            }
+          ).collect(),
           normals: vec!(),
           geometry: vec!(
             Geometry {
@@ -35,9 +95,9 @@ pub fn test_square() {
                   Shape {
                     primitive:
                       Primitive::Triangle(
-                        (x, None, None),
-                        (y, None, None),
-                        (z, None, None)),
+                        (x, Some(x), None),
+                        (y, Some(y), None),
+                        (z, Some(z), None)),
                     groups: vec!(),
                     smoothing_groups: vec!(),
                   })
@@ -45,17 +105,20 @@ pub fn test_square() {
             }
           )
         }
-      ]
+      ],
     };
 
-    let expected = 
-r#"o Square
+    let expected = r#"o Square
 v -1.000000 -1.000000 0.000000
 v 1.000000 -1.000000 0.000000
 v 1.000000 1.000000 0.000000
 v -1.000000 1.000000 0.000000
-f 1 2 3
-f 1 3 4
+vt 0.000000 0.000000
+vt 1.000000 0.000000
+vt 1.000000 1.000000
+vt 0.000000 1.000000
+f 1/1 2/2 3/3
+f 1/1 3/3 4/4
 "#;
 
     let result = obj::export(&set);
@@ -65,8 +128,8 @@ f 1 3 4
 //#[test]
 pub fn test_cube() {
     let set = ObjSet {
-      material_library: None,
-      objects: vec![
+        material_library: None,
+        objects: vec![
         Object {
           name: "Cube".to_owned(),
           vertices:
@@ -119,11 +182,10 @@ pub fn test_cube() {
             }
           )
         }
-      ]
+      ],
     };
 
-    let expected = 
-r#"
+    let expected = r#"
 o Cube
 v -1.000000 -1.000000 1.000000
 v -1.000000 -1.000000 -1.000000
