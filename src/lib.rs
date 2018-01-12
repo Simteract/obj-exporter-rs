86758c8b44b36329d002f0adb82acf7c97975e5c
Adam Rzepka <adam@simteract.com>
2018-01-12 11:25:40 +0100

    Rewrite for speed and better style
    - Serializing directly to `Write` instead of `String`
    - clippy, rustfmt etc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,86 +1,135 @@
-extern crate wavefront_obj;
 extern crate itertools;
+extern crate wavefront_obj;
 
 use std::path::Path;
-use std::io;
-use std::io::Write;
+use std::io::{BufWriter, Result, Write};
 use std::fs;
-use itertools::Itertools;
 
 use wavefront_obj::obj;
-pub use obj::{ObjSet, Object, Shape, Geometry, TVertex, Vertex, Primitive, VertexIndex, TextureIndex, NormalIndex, VTNIndex};
+pub use obj::{Geometry, GroupName, NormalIndex, ObjSet, Object, Primitive, Shape, TVertex,
+              TextureIndex, VTNIndex, Vertex, VertexIndex};
 
-/// Exports ObjSet to String.
-/// Exactly one Geometry is supported for now.
-pub fn export(obj_set: &ObjSet) -> String {
-    let mut exporter = Exporter::new();
-    exporter.export(obj_set)
+/// Exports `ObjSet` to given output.
+pub fn export<W: Write>(obj_set: &ObjSet, output: &mut W) -> Result<()> {
+    Exporter::new(output).export(obj_set)
 }
 
-/// Exports ObjSet to String and writes it to file.
-pub fn export_to_file(obj_set: &ObjSet, path: &Path) -> Result<(), io::Error> {
-    let mut file = fs::File::create(path)?;
-    let content = export(obj_set);
-    file.write(content.as_bytes())?;
-    Ok(())
+/// Exports `ObjSet`to file.
+pub fn export_to_file<P: AsRef<Path>>(obj_set: &ObjSet, path: P) -> Result<()> {
+    let file = fs::File::create(path)?;
+    let mut buffered = BufWriter::new(file);
+    export(obj_set, &mut buffered)
 }
 
-struct Exporter {
-    pub v_base_id: usize,
-    pub uv_base_id: usize,
-    pub n_base_id: usize,
+struct Exporter<'a, W: 'a + Write> {
+    output: &'a mut W,
+    v_base_id: usize,
+    uv_base_id: usize,
+    n_base_id: usize,
 }
 
-impl Exporter {
-    fn new() -> Exporter {
+impl<'a, W: 'a + Write> Exporter<'a, W> {
+    fn new(output: &'a mut W) -> Exporter<W> {
         Exporter {
+            output,
             v_base_id: 1,
             uv_base_id: 1,
             n_base_id: 1,
         }
     }
 
-    fn export(&mut self, obj_set: &ObjSet) -> String {
-        obj_set.objects.iter().map(|o| self.serialize_object(o)).join("")
+    fn export(&mut self, obj_set: &ObjSet) -> Result<()> {
+        for object in &obj_set.objects {
+            self.serialize_object(object)?;
+        }
+        Ok(())
     }
 
-    fn serialize_object(&mut self, object: &Object) -> String {
-        let name = format!("o {}\n", object.name);
-        let vertices = object.vertices.iter().map(|v| Exporter::serialize_vertex(v, "v")).join("");
-        let uvs = object.tex_vertices.iter().map(Exporter::serialize_uv).join("");
-        let normals = object.normals.iter().map(|n| Exporter::serialize_vertex(n, "vn")).join("");
-        assert_eq!(object.geometry.len(), 1);
-        let faces = object.geometry[0].shapes.iter().map(|s| self.serialize_shape(s)).join("");
+    fn serialize_object(&mut self, object: &Object) -> Result<()> {
+        write!(self.output, "o {}\n", object.name)?;
+        self.serialize_vertex_data(object)?;
+        for g in &object.geometry {
+            self.serialize_geometry(g)?;
+        }
         self.update_base_indices(object);
-        format!("{}{}{}{}{}", name, vertices, uvs, normals, faces)
+        Ok(())
     }
 
-    fn serialize_vertex(v: &Vertex, prefix: &str) -> String {
-        format!("{} {:.6} {:.6} {:.6}\n", prefix, v.x, v.y, v.z)
+    fn serialize_vertex_data(&mut self, object: &Object) -> Result<()> {
+        for v in &object.vertices {
+            self.serialize_vertex(v, "v")?;
+        }
+        for uv in &object.tex_vertices {
+            self.serialize_uv(uv)?;
+        }
+        for n in &object.normals {
+            self.serialize_vertex(n, "vn")?
+        }
+        Ok(())
     }
 
-    fn serialize_uv(uv: &TVertex) -> String {
+    fn serialize_geometry(&mut self, geometry: &Geometry) -> Result<()> {
+        for s in &geometry.shapes {
+            self.serialize_shape(s)?;
+        }
+        Ok(())
+    }
+
+    fn serialize_vertex(&mut self, v: &Vertex, prefix: &str) -> Result<()> {
+        write!(self.output, "{} {:.6} {:.6} {:.6}\n", prefix, v.x, v.y, v.z)
+    }
+
+    fn serialize_uv(&mut self, uv: &TVertex) -> Result<()> {
         if uv.w == 0.0 {
-            format!("vt {:.6} {:.6}\n", uv.u, uv.v)
+            write!(self.output, "vt {:.6} {:.6}\n", uv.u, uv.v)
         } else {
-            format!("vt {:.6} {:.6} {:.6}\n", uv.u, uv.v, uv.w)
+            write!(self.output, "vt {:.6} {:.6} {:.6}\n", uv.u, uv.v, uv.w)
         }
     }
 
-    fn serialize_shape(&self, shape: &Shape) -> String {
+    fn serialize_shape(&mut self, shape: &Shape) -> Result<()> {
         match shape.primitive {
-            Primitive::Point(vtn) => format!("p {}\n", self.serialize_vtn(vtn)),
-            Primitive::Line(vtn1, vtn2) => format!("l {} {}\n", self.serialize_vtn(vtn1), self.serialize_vtn(vtn2)),
-            Primitive::Triangle(vtn1, vtn2, vtn3) => format!("f {} {} {}\n", self.serialize_vtn(vtn1), self.serialize_vtn(vtn2), self.serialize_vtn(vtn3)),
+            Primitive::Point(vtn) => {
+                write!(self.output, "p")?;
+                self.serialize_vtn(vtn)?;
+            }
+            Primitive::Line(vtn1, vtn2) => {
+                write!(self.output, "l")?;
+                self.serialize_vtn(vtn1)?;
+                self.serialize_vtn(vtn2)?;
+            }
+            Primitive::Triangle(vtn1, vtn2, vtn3) => {
+                write!(self.output, "f")?;
+                self.serialize_vtn(vtn1)?;
+                self.serialize_vtn(vtn2)?;
+                self.serialize_vtn(vtn3)?;
+            }
         }
+        writeln!(self.output, "")
     }
 
-    fn serialize_vtn(&self, vtn: VTNIndex) -> String {
+    fn serialize_vtn(&mut self, vtn: VTNIndex) -> Result<()> {
         match vtn {
-            (vi, None, None) => format!("{}", vi + self.v_base_id),
-            (vi, Some(ti), None) => format!("{}/{}", vi + self.v_base_id, ti + self.uv_base_id),
-            (vi, Some(ti), Some(ni)) => format!("{}/{}/{}", vi + self.v_base_id, ti + self.uv_base_id, ni + self.n_base_id),
-            (vi, None, Some(ni)) => format!("{}//{}", vi + self.v_base_id, ni + self.n_base_id),
+            (vi, None, None) => write!(self.output, " {}", vi + self.v_base_id),
+            (vi, Some(ti), None) => write!(
+                self.output,
+                " {}/{}",
+                vi + self.v_base_id,
+                ti + self.uv_base_id
+            ),
+            (vi, Some(ti), Some(ni)) => write!(
+                self.output,
+                " {}/{}/{}",
+                vi + self.v_base_id,
+                ti + self.uv_base_id,
+                ni + self.n_base_id
+            ),
+            (vi, None, Some(ni)) => write!(
+                self.output,
+                " {}//{}",
+                vi + self.v_base_id,
+                ni + self.n_base_id
+            ),
         }
     }
 
